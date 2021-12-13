7b72cc5ab7ca5a285942d2063dd44413cf39708a
Facupelli <facundopellicer4@gmail.com>
2021-12-13 16:01:49 -0300

    added post put and delete reviews

--- api/src/restaurants.route.js
+++ api/src/restaurants.route.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import RestaurantsCtrl from './restaurants.controller.js'
+import ReviewsCtrl from './reviews.controller.js'
 
 const router = express.Router()  //acess to express router
 
-router.route('/').get(RestaurantsCtrl.apiGetRestaurants) //demostration route
+router.route('/').get(RestaurantsCtrl.apiGetRestaurants) 
+
+router
+    .route('/review')
+    .post(ReviewsCtrl.apiPostReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
+
 
 export default router
