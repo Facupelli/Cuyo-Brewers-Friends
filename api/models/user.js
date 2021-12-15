785cdc3208984f6743d139eb997d2476e4a71843
Facupelli <facundopellicer4@gmail.com>
2021-12-15 20:31:28 -0300

    user schema added

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  userImg: String,
+  name: { type: String, required: true, min: 2, max: 225 },
+  lastnmae: { type: String, required: true, min: 2, max: 225 },
+  username: { type: String, required: true, min: 2, max: 225 },
+  password: { type: String, required: true, min: 8, max: 225 },
+  email: { type: String, required: true, min: 6, max: 225 },
+  date: { type: Date, default: Date.now },
+});
+
+const userModel = mongoose.model("UserModel", UserSchema);
+module.exports = { userModel };
