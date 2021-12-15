b1b8c1a889ef3e7e7c82e0d369a1108cd442c58a
Facupelli <facundopellicer4@gmail.com>
2021-12-15 17:50:31 -0300

    added 4 styles mock

--- api/mock/recipes/index.js
+++ api/mock/recipes/index.js
@@ -1,9 +1,23 @@
 const americanIPA = require("./americanIPA.js");
 const englishIPA = require("./englishIPA.js");
 const sessionIPA = require("../recipes/sessionIPA.js");
+const porter = require("../recipes/porter.js");
+const creamStout = require("../recipes/creamStout.js");
+const bock = require("../recipes/bock.js");
+const brownIPA = require("../recipes/brownIPA.js");
+
+
 const recipeModel = require("../../models/recipe");
 
-const recipesMock = [...americanIPA, ...englishIPA, ...sessionIPA];
+const recipesMock = [
+  ...americanIPA,
+  ...englishIPA,
+  ...sessionIPA,
+  ...bock,
+  ...creamStout,
+  ...porter,
+  ...brownIPA,
+];
 
 const loadRecipes = () => recipeModel.insertMany(recipesMock);
 
