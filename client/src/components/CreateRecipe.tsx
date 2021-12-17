f6e9cb7fe946a9468f4f187dea3ea8d16ebe7623
Facupelli <facundopellicer4@gmail.com>
2021-12-17 12:53:28 -0300

    form first layout

--- client/src/components/CreateRecipe.tsx
+++ client/src/components/CreateRecipe.tsx
@@ -60,54 +60,84 @@ export const CreateRecipe: React.FC<{}> = () => {
     <div>
       <form>
         {/* ------------    PART 1 ------------------------ */}
-        <label htmlFor="Title">Title</label>
-        <input placeholder="title" {...register("title")} />
-        <label htmlFor="Style">Style</label>
-        <select {...register("style")}>
-          {beerStyles.map((el) => (
-            <option>{el}</option>
-          ))}
-        </select>
-        <label htmlFor="Sub_Category">Sub Category</label>
-        {styleSelected && (
-          <select {...register("sub_category")}>
-            {beerSubCategories[0].map((el) => (
-              <option>{el.name}</option>
-            ))}
-          </select>
-        )}
-        <label htmlFor="Brewery">Brewery</label>
-        <input placeholder="brewery" {...register("brewery")} />
+        <div className="grid grid-cols-2 p-8">
+          <div className="col-span-2 p-4">
+            <label htmlFor="Title">Title</label>
+            <input placeholder="title" {...register("title")} />
+
+            <label htmlFor="Brewery">Brewery</label>
+            <input placeholder="brewery" {...register("brewery")} />
+          </div>
+
+          <div className="grid-cols-1 flex flex-col p-4">
+            <div className="flex">
+              <label htmlFor="Batch_Size">Batch Size</label>
+              <input placeholder="20" {...register("parameters.batch_size")} />
+            </div>
+
+            <div>
+              <label htmlFor="Pre_Boil_Size">Pre Boil Size</label>
+              <input
+                placeholder="30"
+                {...register("parameters.pre_boil_size")}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="Boil_Time">Boil Time</label>
+              <input placeholder="60" {...register("parameters.boil_time")} />
+            </div>
+          </div>
+
+          <div className="grid-cols-1 flex flex-col p-4">
+            <div className="flex">
+              <label htmlFor="Style">Style</label>
+              <select {...register("style")}>
+                {beerStyles.map((el) => (
+                  <option>{el}</option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="Sub_Category">Sub Category</label>
+              {styleSelected && (
+                <select {...register("sub_category")}>
+                  {beerSubCategories[0].map((el) => (
+                    <option>{el.name}</option>
+                  ))}
+                </select>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Efficiency">Efficiency</label>
+              <input placeholder="70" {...register("parameters.efficiency")} />
+            </div>
+
+            <div>
+              <label htmlFor="Mash_Ph">Mash Ph</label>
+              <input placeholder="5.4" {...register("parameters.mash_ph")} />
+            </div>
+          </div>
+        </div>
 
         {/* -------------------    PARAMETERS ------------------------ */}
-        <label htmlFor="Brewery">Boil Time</label>
-        <input placeholder="60" {...register("parameters.boil_time")} />
 
-        <label htmlFor="Brewery">Boil Time</label>
-        <input placeholder="20" {...register("parameters.batch_size")} />
-
-        <label htmlFor="Brewery">Boil Time</label>
-        <input placeholder="30" {...register("parameters.pre_boil_size")} />
-
-        <label htmlFor="Brewery">Boil Time</label>
-        <input placeholder="pre boil gravity" {...register("parameters.pre_boil_gravity")} />
-
-        <label htmlFor="Mash_Ph">Boil Time</label>
-        <input placeholder="5.4" {...register("parameters.mash_ph")} />
-
-        <label htmlFor="Efficiency">Efficiency</label>
-        <input placeholder="70" {...register("parameters.efficiency")} />
+        {/* <label htmlFor="Pre_Boil_Gravity">Pre Boil Gravity</label>
+        <input
+          placeholder="pre boil gravity"
+          {...register("parameters.pre_boil_gravity")}
+        /> */}
 
         {/* --------------------    CHARACTERISTICS ------------------------ */}
-        <label htmlFor="Original_Gravity">Original Gravity</label>
-        <label htmlFor="Final_Gravity">Final Gravity</label>
-        <label htmlFor="ABV">ABV</label>
-        <label htmlFor="IBU">IBU</label>
-        <label htmlFor="SRM">SRM</label>
-
-        
-
-        
+        <div className="flex gap-10 justify-center p-8">
+          <p>Original Gravity</p>
+          <p>Final Gravity</p>
+          <p>ABV</p>
+          <p>IBU</p>
+          <p>SRM</p>
+        </div>
 
         <button type="submit">Submit</button>
       </form>
