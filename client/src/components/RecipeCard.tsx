6cf5aa7fa4d1c9c33744de3a97ba12eea308390f
Facupelli <facundopellicer4@gmail.com>
2021-12-18 23:47:36 -0300

    recipe card added info

--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -8,20 +8,43 @@ type Props = {
 
 export const RecipeCard: React.FC<Props> = ({ recipe }) => {
   return (
-    <div className="w-72 bg-amber-100 rounded m-2 p-4">
+    <div className="bg-amber-100 rounded m-2 p-6">
       <div>
         <div>
-          <Link to="#recipeid">{recipe.title}</Link>
+          <Link to="recipe/:id">
+            <p className="text-gray-800 font-semibold">{recipe.title}</p>
+          </Link>
         </div>
-        <div>{recipe.style}</div>
+        <div>
+          <p className="text-gray-800">{recipe.style.split(". ")[1]}</p>
+        </div>
       </div>
 
       <div className="flex justify-start gap-6">
-        <div>{recipe.characteristics.original_gravity}</div>
-        <div>{recipe.characteristics.final_gravity}</div>
-        <div>{recipe.characteristics.alcohol_by_volume}</div>
-        <div>{recipe.characteristics.ibu}</div>
-        <div>{recipe.characteristics.srm}</div>
+        <div className="flex items-center gap-2">
+          <p className="text-gray-600">OG:</p>
+          <p>{recipe.characteristics.original_gravity}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <p className="text-gray-600">FG:</p>
+          <p>{recipe.characteristics.final_gravity}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <p className="text-gray-600">ABV:</p>
+          <p>{recipe.characteristics.alcohol_by_volume}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <p className="text-gray-600">IBU:</p>
+          <p>{recipe.characteristics.ibu}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <p className="text-gray-600">SRM:</p>
+          <p>{recipe.characteristics.srm}</p>
+        </div>
       </div>
     </div>
   );
