de5b3bfe44163057cfea5573fc0651a218dd0813
Facupelli <facundopellicer4@gmail.com>
2022-01-02 18:09:09 -0300

    beer fermentables json

---
+++ client/src/media/beer_ingredients/fermentables/adjuncts.js
@@ -0,0 +1,28 @@
+export const adjuncts = [
+  {
+    name: "Barley Hulls",
+    yield: 0.0,
+    color: 0,
+    max_in_batch: 0.05,
+    potential: 1.0,
+    notes: "Neutral hulls used to improve lautering.",
+  },
+
+  {
+    name: "Grits",
+    yield: 0.8,
+    color: 1,
+    recommend_mash: true,
+    max_in_batch: 0.1,
+    potential: 1.037,
+    notes: "Imparts a corn and grain taste",
+  },
+  {
+    name: "Rice Hulls",
+    yield: 0.0,
+    color: 0,
+    max_in_batch: 0.05,
+    potential: 1.0,
+    notes: "Neutral hulls used to improve lautering.",
+  },
+];
---
+++ client/src/media/beer_ingredients/fermentables/index.js
@@ -0,0 +1,10 @@
+import { adjuncts } from "./adjuncts";
+import { sugars } from "./sugars";
+import { grains } from "./grains";
+
+export const fermentables = [
+  ...adjuncts,
+  ...sugars,
+  ...grains,
+];
+
