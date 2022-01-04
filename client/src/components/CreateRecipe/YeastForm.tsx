ccd2da75c6ab157a43e5f1c561d6dfe8cf2fb280
Facupelli <facundopellicer4@gmail.com>
2022-01-04 12:18:19 -0300

    yeast select and quantitu

--- client/src/components/CreateRecipe/YeastForm.tsx
+++ client/src/components/CreateRecipe/YeastForm.tsx
@@ -1,42 +1,103 @@
 import React, { useEffect } from "react";
 import { Controller, useFormContext } from "react-hook-form";
-import {FaMicroscope} from 'react-icons/fa'
+import { FaMicroscope } from "react-icons/fa";
+import { fermentis } from "../../media/beer_ingredients/yeast/fermentis";
 
 type Props = {
-    setYeastAtt: React.Dispatch<React.SetStateAction<any>>;
-}
+  setYeastAtt: React.Dispatch<React.SetStateAction<any>>;
+};
 
-export const YeastForm: React.FC<Props> = ({setYeastAtt}) => {
-  const { control, watch } = useFormContext();
+export const YeastForm: React.FC<Props> = ({ setYeastAtt }) => {
+  const { control, watch, register } = useFormContext();
 
-  const att = watch('ingredients.yeast.attenuation')
+  const att = watch("ingredients.yeast.attenuation");
+
+  const yeastSelected = watch("ingredients.yeast.name");
+
+  const getYeastAtt = () => {
+    return fermentis
+      .filter((el) => el.name === yeastSelected)
+      .map((el) => el.attenuation)[0];
+  };
+
+  const attAcordToYeast = getYeastAtt();
 
   useEffect(() => {
-    setYeastAtt(att)
-  },[att, setYeastAtt])
+    setYeastAtt(att);
+  }, [att, setYeastAtt]);
 
   return (
     <div className="m-8 p-4 bg-gray-100">
       <div className="flex justify-center items-baseline gap-2 border-b-2 border-blueLight">
         <p className="font-semibold text-2xl pb-4">Yeast</p>
         <div className="text-xl">
-            <FaMicroscope />
+          <FaMicroscope />
         </div>
       </div>
+
       <div className="flex items-center my-4 gap-4">
-        <label className="text-gray-700 text-md font-semibold ">Attenuation (%)</label>
-        <Controller
-          name={`ingredients.yeast.attenuation`}
-          defaultValue={75}
-          control={control}
-          render={({ field }) => (
-            <input
-              placeholder="0"
-              {...field}
-              className="p-2 text-center w-14 shadow appearance-none rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          )}
-        />
+        <label className="text-gray-700 text-md font-semibold ">Yeast</label>
+        <select
+          {...register(`ingredients.yeast.name`)}
+          defaultValue=""
+          className="bg-white border border-blue-200  text-gray-700 p-2 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+        >
+          <option value="" disabled>
+            Select Yeast
+          </option>
+          {fermentis.map((el) => (
+            <option key={el.name}>{el.name}</option>
+          ))}
+        </select>
+      </div>
+
+      {attAcordToYeast && (
+        <div className="flex items-center my-4 gap-2">
+          <label className="text-gray-500 text-xs font-semibold ">
+            Attenuation of yeast selected
+          </label>
+          <p className="text-gray-500 text-xs font-semibold ">
+            {attAcordToYeast * 100} %
+          </p>
+        </div>
+      )}
+
+      <div className="grid grid-cols-2">
+        <div className="cols-span1 flex items-center gap-4">
+          <label className="text-gray-700 text-md font-semibold ">
+            Attenuation (%)
+          </label>
+          <Controller
+            name={`ingredients.yeast.attenuation`}
+            defaultValue={75}
+            control={control}
+            render={({ field }) => (
+              <input
+                placeholder="0"
+                {...field}
+                className="p-2 text-center w-14 shadow appearance-none rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            )}
+          />
+        </div>
+
+        <div className="col-span-1 flex items-center gap-4">
+          <label className="text-gray-700 text-md font-semibold ">
+            Quantity (g)
+          </label>
+          <Controller
+            name={`ingredients.yeast.quantity`}
+            defaultValue={0}
+            control={control}
+            render={({ field }) => (
+              <input
+                placeholder="0"
+                {...field}
+                className="p-2 text-center w-14 shadow appearance-none rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            )}
+          />
+        </div>
       </div>
     </div>
   );
---
+++ client/src/media/beer_ingredients/yeast/fermentis.ts
@@ -0,0 +1,136 @@
+export const fermentis = [
+  {
+    min_temperature: 15.0,
+    name: "K_97 SafAle German Ale",
+    max_temperature: 24.0,
+    type: "Ale",
+    best_for: "High attenuation ales, wheat beers and weizens.",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.80,
+    notes:
+      "Low sedimentation yeast, sometimes used in open fermentation. Good for wheat beers, weizens and light ales.",
+    flocculation: "Medium",
+    form: "Dry",
+    product_id: "K_97",
+  },
+
+  {
+    min_temperature: 15.0,
+    name: "S_04 SafAle English Ale",
+    max_temperature: 24.0,
+    type: "Ale",
+    best_for: "Great general purpose ale yeast.",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.74,
+    notes:
+      "Fast starting, fast fermenting yeast. Quick attenuation helps to produce a clean, crisp, clear ale. Can be used in a wide range of ales.",
+    flocculation: "Medium",
+    form: "Dry",
+    product_id: "S_04",
+  },
+
+  {
+    min_temperature: 8.89,
+    name: "S_189 SafLager German Lager",
+    max_temperature: 13.33,
+    type: "Lager",
+    best_for: "Wide range of lagers and pilsners.",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.80,
+    notes:
+      "Popular lager yeast strain. Produces wide range of continental lagers and pilsners. Clean finish.",
+    flocculation: "High",
+    form: "Dry",
+    product_id: "S_189",
+  },
+
+  {
+    min_temperature: 7.78,
+    name: "S_23 SafLager West European Lager",
+    max_temperature: 10.0,
+    type: "Lager",
+    best_for: "German style Lagers and Pilsners.",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.80,
+    notes:
+      "German lager yeast strain. Performs well at low temperature. High flocculation and attenuation for a clean German finish.",
+    flocculation: "High",
+    form: "Dry",
+    product_id: "S_23",
+  },
+
+  {
+    min_temperature: 18.33,
+    name: "S_33 SafBrew Ale",
+    max_temperature: 22.22,
+    type: "Ale",
+    best_for: "Most ales.",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.68,
+    notes:
+      "General purpose ale yeast, widely used. Very consistent, clean finish. High attenuation and good flavor profile.",
+    flocculation: "Medium",
+    form: "Dry",
+    product_id: "S_33",
+  },
+
+  {
+    min_temperature: 15.56,
+    name: "T_58 SafBrew Specialty Ale",
+    max_temperature: 22.22,
+    type: "Ale",
+    best_for: "Complex ales.",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.72,
+    notes:
+      "Estery, somewhat spicy ale yeast. Solid yeast formation at end of fermentation. Widely used for bottle and cask conditioning.",
+    flocculation: "Medium",
+    form: "Dry",
+    product_id: "T_58",
+  },
+
+  {
+    min_temperature: 15.0,
+    name: "US_05 Safale American",
+    max_temperature: 23.89,
+    type: "Ale",
+    best_for: "American ale, other clean finish ales",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.78,
+    notes:
+      "American ale yeast that produces well balanced beers with low diacetyl and a very clean, crisp end palate.",
+    flocculation: "Medium",
+    form: "Dry",
+    product_id: "US_05",
+  },
+
+  {
+    min_temperature: 8.89,
+    name: "W_34/70 Saflager Lager",
+    max_temperature: 15.0,
+    type: "Lager",
+    best_for: "European lagers",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.80,
+    notes:
+      "A famous yeast strain from Weihenstephan Germany used worldwide in brewing. Their most popular strain for lagers.",
+    flocculation: "High",
+    form: "Dry",
+    product_id: "W_34/70",
+  },
+
+  {
+    min_temperature: 15.0,
+    name: "WB_06 Safbrew Wheat",
+    max_temperature: 23.89,
+    type: "Wheat",
+    best_for: "Wheat beers",
+    laboratory: "DCL/Fermentis",
+    attenuation: 0.86,
+    notes:
+      "A specialty yeast for wheat beer fermentation. The yeast produces a subtle estery and phenlol flavor typical of wheat beers.",
+    flocculation: "Medium",
+    form: "Dry",
+    product_id: "WB_06",
+  },
+];
