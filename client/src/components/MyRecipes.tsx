d161e88cdd6ceddc9a772690e0457e2a73387112
Facupelli <facundopellicer4@gmail.com>
2022-01-11 16:22:27 -0300

    myrecipes responsive

--- client/src/components/MyRecipes.tsx
+++ client/src/components/MyRecipes.tsx
@@ -17,7 +17,7 @@ export const MyRecipes: React.FC = (z) => {
       <div className="mt-16">
         {ownRecipes.length > 0 ? (
           <>
-            <div className="grid grid-cols-10 m-4 px-4 ">
+            <div className="hidden md:grid grid-cols-10 m-4 px-4 ">
               <p className="col-span-2 font-semibold text-sm text-brown1">
                 Recipe
               </p>
@@ -44,28 +44,29 @@ export const MyRecipes: React.FC = (z) => {
             {ownRecipes.map((el) => (
               <div
                 key={el._id}
-                className="grid grid-cols-10 mx-4 py-6 px-4 bg-gray-100 mb-2 border-b border-blueLight"
+                className="grid grid-cols-10 gap-y-3 mx-4 py-6 px-4 bg-gray-100 mb-2 border-b border-blueLight"
               >
-                <div className="col-span-2">
+                <div className="col-span-3 md:col-span-2">
                   <Link to={`/recipe/${el._id}`}>
                     <p className="font-semibold">{el.recipe.title}</p>
                   </Link>
                 </div>
-                <div className="col-span-3 flex gap-4">
+                <div className="col-span-7 md:col-span-3 flex gap-4">
                   <p>{el.recipe.style.label}</p>
                   <p className="font-semibold text-blueDark">
                     {el.recipe.sub_category}
                   </p>
                 </div>
-                <p className="col-span-1">
+                <p className="col-span-3 md:col-span-1">
                   {el.recipe.parameters.batch_size} Liters
                 </p>
-                <p className="col-span-1">
+                <p className="col-span-2 md:col-span-1">
                   {el.recipe.characteristics.alcohol_by_volume}%
                 </p>
-                <p className="col-span-1">{el.recipe.characteristics.ibu}</p>
-                <p className="col-span-1">{el.recipe.characteristics.srm}</p>
-                <p className="col-span-1">{el.date}</p>
+                <p className="col-span-1 md:hidden text-brownLight">IBU:</p>
+                <p className="col-span-2 md:col-span-1">{el.recipe.characteristics.ibu}</p>
+                <p className="hidden md:block md:col-span-1">{el.recipe.characteristics.srm}</p>
+                <p className="col-span-2 md:col-span-1">{el.date}</p>
               </div>
             ))}
           </>
