2998ca69d689b8f406e22d0694899d906f3cdfbb
Facupelli <facundopellicer4@gmail.com>
2022-01-12 21:06:01 -0300

    userprofile responsive

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -26,7 +26,7 @@ export const UserProfile: React.FC = () => {
   return (
     <div>
       <NavBar route="userprofile" />
-      <div className="max-w-7xl mx-auto mt-8">
+      <div className="max-w-7xl md:mx-auto ml-4 mt-8">
         <div>
           <div className="flex gap-4 items-baseline">
             <span className="text-4xl pb-3 border-b-2 border-blueLight">
@@ -34,20 +34,20 @@ export const UserProfile: React.FC = () => {
             </span>
             <p className="text-gray-500 text-lg">homebrewer</p>
           </div>
-          <div className="grid grid-cols-2 gap-x-8 mt-12">
-            <div className="col-span-1">
-              <p className="font-semibold text-xl ml-2">RECIPES:</p>
+          <div className="grid grid-cols-2 gap-y-8 md:gap-x-8 mt-12">
+            <div className="col-span-2 md:col-span-1">
+              <p className="font-semibold text-xl md:ml-2">RECIPES:</p>
               {userProfile &&
                 userProfile.userProfile.ownRecipes.map((recipe, i) => (
                   <RecipeCard key={i} recipe={recipe.recipe} id={recipe._id} />
                 ))}
             </div>
-            <div className="col-span-1">
+            <div className="col-span-2 md:col-span-1">
               <p className="font-semibold text-xl">ARTICLES:</p>
               {userProfile?.userProfile.ownBlogs.map((el, i) => (
                 <div
                   key={i}
-                  className="flex gap-x-3 bg-blue-50 rounded shadow hover:shadow-none p-4 my-3"
+                  className="flex md:gap-x-3 bg-blue-50 rounded shadow hover:shadow-none p-4 my-3"
                 >
                   <Link to={`/blogdetail/${el._id}`}>
                     <p>{el.blog_title}</p>
